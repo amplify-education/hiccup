44917f56faaa8fdf33f92026621df8c0ab49be7b
zackjp <zackjp87@gmail.com>
2014-02-17 12:59:32 -0500

    Implement post() for HiccupClient

--- src/main/java/com/amplify/hiccup/client/HiccupClient.java
+++ src/main/java/com/amplify/hiccup/client/HiccupClient.java
@@ -1,18 +1,32 @@
 package com.amplify.hiccup.client;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import com.amplify.hiccup.service.JsonConverter;
 
 public class HiccupClient {
 
-    private final Context context;
+    private static final String METHOD = "method";
+    private static final String BODY = "body";
 
-    public HiccupClient(Context context) {
+    private final Context context;
+    private final JsonConverter jsonConverter;
+
+    public HiccupClient(Context context, JsonConverter jsonConverter) {
         this.context = context;
+        this.jsonConverter = jsonConverter;
     }
 
     public Cursor get(Uri uri) {
         return context.getContentResolver().query(uri, null, null, null, null);
+    }
+
+    public Uri post(Uri uri, Object model) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(METHOD, "POST");
+        contentValues.put(BODY, jsonConverter.toJson(model));
+        return context.getContentResolver().insert(uri, contentValues);
     }
 }
--- src/test/java/com/amplify/hiccup/client/HiccupClientTest.java
+++ src/test/java/com/amplify/hiccup/client/HiccupClientTest.java
@@ -1,24 +1,29 @@
 package com.amplify.hiccup.client;
 
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import com.amplify.hiccup.service.JsonConverter;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.robolectric.RobolectricTestRunner;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 @RunWith(RobolectricTestRunner.class)
 public class HiccupClientTest {
+
+    private static final String METHOD = "method";
+    private static final String BODY = "body";
 
     private HiccupClient hiccupClient;
 
@@ -26,6 +31,10 @@ public class HiccupClientTest {
     private Context context;
     @Mock
     private ContentResolver contentResolver;
+    @Mock
+    private Uri uri;
+    @Mock
+    private JsonConverter jsonConverter;
 
     @Before
     public void setUp() throws Exception {
@@ -33,17 +42,50 @@ public class HiccupClientTest {
 
         given(context.getContentResolver()).willReturn(contentResolver);
 
-        hiccupClient = new HiccupClient(context);
+        hiccupClient = new HiccupClient(context, jsonConverter);
     }
 
     @Test
     public void shouldSendUriRequestViaContentResolver() {
-        Uri uri = mock(Uri.class);
         Cursor expectedCursor = mock(Cursor.class);
         when(contentResolver.query(uri, null, null, null, null)).thenReturn(expectedCursor);
 
         Cursor actualCursor = hiccupClient.get(uri);
 
         assertThat(actualCursor, is(expectedCursor));
+    }
+
+    @Test
+    public void shouldInsertAsPostToContentProviderWithPostMethod() {
+        hiccupClient.post(uri, new Object());
+
+        ArgumentCaptor<ContentValues> captor = ArgumentCaptor.forClass(ContentValues.class);
+        verify(contentResolver).insert(eq(uri), captor.capture());
+        ContentValues contentValues = captor.getValue();
+        assertThat(contentValues.getAsString(METHOD), is("POST"));
+    }
+
+    @Test
+    public void shouldInsertAsPostToContentProviderWithJsonModel() {
+        Object model = new Object();
+        String expectedJson = "Lois, this is not my Batman glass.";
+        when(jsonConverter.toJson(model)).thenReturn(expectedJson);
+
+        hiccupClient.post(uri, model);
+
+        ArgumentCaptor<ContentValues> captor = ArgumentCaptor.forClass(ContentValues.class);
+        verify(contentResolver).insert(eq(uri), captor.capture());
+        ContentValues contentValues = captor.getValue();
+        assertThat(contentValues.getAsString(BODY), is(expectedJson));
+    }
+
+    @Test
+    public void shouldReturnUriFromInsertAsPostToContentProvider() {
+        Uri expectedUri = mock(Uri.class);
+        when(contentResolver.insert(eq(uri), any(ContentValues.class))).thenReturn(expectedUri);
+
+        Uri actualUri = hiccupClient.post(uri, new Object());
+
+        assertThat(actualUri, is(expectedUri));
     }
 }
