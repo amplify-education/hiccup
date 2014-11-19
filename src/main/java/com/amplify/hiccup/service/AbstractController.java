55eeda546bd75f1b4d3c9c5abcec1825007bc5be
zackjp <zackjp87@gmail.com>
2014-11-18 23:44:24 -0500

    Create AbstractController to help handle converting requests/responses to/from POJOs

---
+++ src/main/java/com/amplify/hiccup/service/AbstractController.java
@@ -0,0 +1,31 @@
+package com.amplify.hiccup.service;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+public abstract class AbstractController<R> implements Controller {
+
+    private final ContentAdapter contentAdapter;
+    private final Class<R> modelClass;
+
+    public AbstractController(ContentAdapter contentAdapter, Class<R> modelClass) {
+        this.contentAdapter = contentAdapter;
+        this.modelClass = modelClass;
+    }
+
+    @Override
+    public final Cursor get(Uri uri) {
+        Iterable<R> models = handleGet(uri);
+        return contentAdapter.toCursor(models);
+    }
+
+    @Override
+    public final Uri post(Uri uri, ContentValues contentValues) {
+        R model = (R) contentAdapter.toModel(contentValues, modelClass);
+        return handlePost(uri, model);
+    }
+
+    protected abstract Iterable<R> handleGet(Uri uri);
+    protected abstract Uri handlePost(Uri uri, R model);
+}
